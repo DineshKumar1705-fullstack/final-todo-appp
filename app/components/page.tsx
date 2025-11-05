a4ab50b209939d631878ba287ac794cd6b438538
Dinesh Kumar <mbadinesh1705@gmail.com>
2025-11-05 15:07:40 +0530

    updated new features

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -4,22 +4,22 @@ import Link from 'next/link'
 const SideBar = () => {
   return (
     <div>
-        <aside className="w-full bg-gray-100 border-r p-5 shadow-md flex flex-col justify-between">
+        <aside className="w-full bg-black border-r p-5 shadow-md flex flex-col justify-between">
         <div className="flex flex-col justify-between">
           
           <ul className="space-x-4 flex flex-row justify-end gap:4">
             <li>
-              <Link href="/todo" className="text-black hover:text-blue-600 font-medium">
+              <Link href="/todo" className=" hover:text-blue-600 font-medium text-white bg-gray-700 p-2 border-none rounded-lg">
                 🏠 Todo List
               </Link>
             </li>
             <li>
-              <Link href="/articles" className=" text-black hover:text-blue-600 font-medium">
+              <Link href="/articles" className=" text-black hover:text-blue-600 font-medium text-white bg-gray-700 p-2 border-none rounded-lg">
                 📰 News
               </Link>
              </li>
              <li> 
-              <Link href="/feedback" className="hover:text-blue-600 font-medium text-black" >📠FeedBack</Link>
+              <Link href="/feedback" className="hover:text-blue-600 font-medium text-black text-white bg-gray-700 p-2 border-none rounded-lg" >📠FeedBack</Link>
             </li>
           </ul>
         </div>
